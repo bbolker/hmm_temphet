c480d323ffa067000845119ce8ceb2fbde5a3897
mikelin <wzmichael.li@gmail.com>
2015-10-25 22:22:05 -0400

    fix typo

--- fitblock.R
+++ fitblock.R
@@ -16,7 +16,7 @@ fitblock <- function(state,cat,seed=2830){
 
 fitblock3s <- fitblock(3,cat)
 fitblock4s <- fitblock(4,cat)
-fitblock5s <- fitblock(35cat)
+fitblock5s <- fitblock(5,cat)
 fitblock6s <- fitblock(6,cat)
 
 save(list(c(fitblock3s,fitblock4s,fitblock5s,fitblock6s)),file="hmmblock.RData")
--- fithomo.R
+++ fithomo.R
@@ -14,7 +14,7 @@ fithomo <- function(state,cat){
 
 fithomo3s <- fithomo(3,cat)
 fithomo4s <- fithomo(4,cat)
-fithomo5s <- fithomo(35cat)
+fithomo5s <- fithomo(5,cat)
 fithomo6s <- fithomo(6,cat)
 
 save(list(c(fithomo3s,fithomo4s,fithomo5s,fithomo6s)),file="hmmhomo.RData")
