00935c6b025e1b90ce48b15b2969b76e433663f3
mli2830 <wzmichael.li@gmail.com>
2016-06-15 00:02:37 -0400

    adding hourly

---
+++ hmmhourly.R
@@ -0,0 +1,57 @@
+library(depmixS4)
+library(dplyr)
+library(plyr)
+
+files <- commandArgs(trailingOnly = TRUE)
+catid <- unlist(strsplit(files[1],split="[.]"))[2]
+dat <- readRDS(files[1])
+source(files[2])
+
+fithmmhourly <- function(state,cat,seed=NULL){
+  if(!is.null(seed))set.seed(seed)
+  model <- depmix(LogDist~1,
+                  data=cat,
+                  nstate=state,
+                  transition=~factor(Time),
+                  family=gaussian())
+  set.seed(seed)
+  fitmodel <- fit(model,emcontrol=em.control(maxit=500))
+  return(fitmodel)
+}
+
+simhmmhourly <- function(state,cat,fit){
+  model <- depmix(LogDist~1,
+                  data=cat,
+                  nstate=state,
+                  transition=~factor(Time),
+                  family=gaussian())
+  model<-setpars(model,getpars(fit))
+  sim <- simhmm(model)
+  df <- data.frame(obs= sim@response[[1]][[1]]@y,states=sim@states)
+  return(df)}
+
+sumdf <- function(lst){
+  BIC <- ldply(lst,BIC)
+  nstates <-ldply(lst,nstates)
+  para <- ldply(lst,freepars)
+  model <- c('HMM + THhourly','HMM + THhourly')
+  type <- c('HMM + TH','HMM + TH')
+  temp <- data.frame(BICS=BIC$V1,nstates=nstates$V1,parameters=para$V1,model,type)
+  return(temp)
+}
+
+fithmmhourly3s <- fithourly(3,dat,seed=1)
+fithmmhourly4s <- fithourly(4,dat,seed=1)
+
+
+fitlist <- list(fithmmhourly3s,fithmmhourly4s)
+
+catsum <- sumdf(fitlist)
+simdf <- data.frame(hmm3S=simhmmhourly(3,dat,fithmm3s)[2]
+                    , hmm3obs=simhmmhourly(3,dat,fithmm3s)[1]
+                    , hmm4S=simhmmhourly(4,dat,fithmm4s)[2]
+                    , hmm4obs=simhmmhourly(4,dat,fithmm4s)[1]
+)
+
+saveRDS(catsum,file=paste("cat",catid,"hmmhourly","sum","RDS",sep="."))
+saveRDS(simdf,file=paste("cat",catid,"hmmhourly","sim","RDS",sep="."))
